ae9b16716e16eb6682ae16126d1bd49e0b761b16
Kevin Brack <brackkevin@gmail.com>
2019-06-09 21:04:48 -0500

    Created Project component

---
+++ components/Projects/Project.jsx
@@ -0,0 +1,28 @@
+import "./Projects.scss";
+
+const Project = props => {
+  const { project } = props;
+  return (
+    <>
+      <div className="grid-image">
+        <img src={project.img} alt={project.alt} />
+      </div>
+      <div className="grid-desc">
+        <a href={project.href} target="blank">
+          <h4>{project.heading}</h4>
+        </a>
+        <ul>
+          {project.techStack.length > 0 &&
+            project.techStack.map((item, i) => <li key={i}>[ {item} ]</li>)}
+        </ul>
+        {project.bullets.length > 0 &&
+          project.bullets.map((item, i) => <p key={i}>{item}</p>)}
+        <a href={project.source} className="source" target="blank">
+          SOURCE CODE
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Project;
