f5aa9d41e905a2dda8f7b62ec9f567e3d885d415
Kevin Brack <brackkevin@gmail.com>
2019-06-25 08:54:38 -0500

    Added details for moodi and riy

--- components/Projects/project-data.js
+++ components/Projects/project-data.js
@@ -1,12 +1,40 @@
 const projectsData = [
   {
+    img: "/static/images/projects/rate-it-yourself.jpg",
+    alt: "rate it yourself",
+    href: "https://how-tutor.netlify.com/",
+    heading: "Rate It Yourself",
+    techStack: ["React","Redux","Express","PassportJS","Postgres","Bootstrap"],
+    bullets: [
+      "Team Lead overseeing the development by 4 web developers, 1 iOS developer, and 1 UI/UX designer",
+      "Created reusable components, with Bootstrap elements to match the designers vision",
+      "Used Filepond to implement drag and drop image uploads",
+      "Redux used to handle all data manipulation and actions in a 1 way data flow"
+    ],
+    source: "https://github.com/labs13-how-to"
+  },
+  {
+    img: "/static/images/projects/moodi-beats.jpg",
+    alt: "moody beats",
+    href: "https://moodibeats.com/",
+    heading: "Moodi Beats",
+    techStack: ["React","Express","Django","StyledComponents","Postgres"],
+    bullets: [
+      "Team Lead overseeing the development by 5 web developers, and 2 data scientists",
+      "Google API used for authentication and YouTube integration",
+      "Django back end created with DS models to sort songs by mood",
+      "Chronjob set up to automatically search for new royalty free instrumentals, analyze mood, and add to collection autonomously over time "
+    ],
+    source: "https://github.com/labs11-studentdata"
+  },
+  {
     img: "/static/images/projects/student-data.jpg",
     alt: "student data",
     href: "https://www.studentdata.dev",
     heading: "studentdata.dev",
     techStack: ["React", "Express", "MySql", "AWS", "Stripe"],
     bullets: [
-      "Executed in 5 weeks, with a team of 5 full stack web developersv",
+      "Executed in 5 weeks, with a team of 5 full stack web developers",
       "OAuth and Passport.js used for authentication with Google",
       "Stripe used to process payments",
       "MySQL database volume managed with Amazon Web Services"
