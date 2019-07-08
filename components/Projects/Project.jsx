b372a95820a0f738bb9dda3fd9d36c0966d4ea5e
Kevin <brackkevin@gmail.com>
2019-07-08 12:18:33 -0500

    Made the images become links in projects section

--- components/Projects/Project.jsx
+++ components/Projects/Project.jsx
@@ -4,9 +4,11 @@ const Project = props => {
   const { project } = props;
   return (
     <>
-      <div className="grid-image">
-        <img src={project.img} alt={project.alt} />
-      </div>
+      <a href={project.href} target="blank">
+        <div className="grid-image">
+          <img src={project.img} alt={project.alt} />
+        </div>
+      </a>
       <div className="grid-desc">
         <a href={project.href} target="blank">
           <h4>{project.heading}</h4>
